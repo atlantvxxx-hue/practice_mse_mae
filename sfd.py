60c5e5a33dbc99bf4643b96d362707cde8ef18f2
atlantvxxx <atlantvxxx@gmail.com>
2025-12-04 21:16:26 +0300

    Initial commit

---
+++ sfd.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+x = np.array([1, 2, 3]) #кол-во кг
+y = np.array([100, 190, 280]) # реальная цена
+
+# начальные предсказания
+w = 70
+b = 30
+
+#скорость обучения
+
+learning_rate = 0.2
+
+print("НАЧАЛО ОБУЧЕНИЯ: Угадываем цену яблок")
+
+for iteration in range(5):
+    print('='*30)
+    print('итераций номер {},  показатели w = {}, b = {}'.format(iteration, w, b))
+
+    y_pred = w*x + b
+    print('предсказание {}'.format(y_pred))
+
+    errors = y_pred-y
+    print('ошибки {}'.format(errors))
+
+    mse = np.mean(errors ** 2)
+    print('Mse = {}'.format(mse))
+
+    n = len(x)
+
+    graw_w = 2/n * np.sum(errors * x)
+    graw_b = 2 / n * np.sum(errors)
+
+    w = w - learning_rate * graw_w
+    b = b - learning_rate * graw_b
+
+    final_predict = w * x + b
+    print(final_predict)
+
+for i in range(len(x)):
+    error = final_predict[i] - y[i]
+    print('')
+    print(f'{x[i]:<10}, {y[i]:<10}, {error:<10}')
+print('{:.2f}  и  {:.2f}'.format(w, b))
---
+++ впы.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+x = np.array([1, 3, 5, 7, 10]) # расстояние в км
+y = np.array([15, 25, 35, 45, 60 ]) # время доставки в минутах
+
+w = 20
+b = 5
+
+#скорость обучения
+learning_rate = 0.01
+
+for iteration in range(1,6):
+    print('итерация номер {}, показатель w = {:.2f} и b = {:.2f}'.format(iteration, w, b))
+
+    y_pred = w * x + b
+    print('предсказание {}'.format(y_pred))
+
+    errors = y_pred - y
+    print('ошибка {}'.format(errors))
+
+    mae = abs(np.mean(errors))
+    print('mae = {}'.format(mae))
+
+    n = len(x)
+    graw_w = 2/n * np.sum(errors * x)
+    graw_b = 2/n * np.sum(errors)
+
+    w = w - learning_rate * graw_w
+    b = b - learning_rate * graw_b
